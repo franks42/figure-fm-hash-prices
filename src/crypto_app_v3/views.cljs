021f139c5ed8f4c4ea7f93562f9ea09ca7aa178f
FrankS <frank.siebenlist@gmail.com>
2025-09-20 18:25:16 -0700

    Bump version to v3.4.0-trades-restored
    - Reflects addition of 24h trades count feature
    - Data source moved to price line

--- src/crypto_app_v3/views.cljs
+++ src/crypto_app_v3/views.cljs
@@ -5,7 +5,7 @@
             [crypto-app-v3.portfolio-atoms :as portfolio-atoms]))
 
 ;; Import version from core
-(def VERSION "v3.3.3-timestamp-top-right-local")
+(def VERSION "v3.4.0-trades-restored")
 
 ;; Copy V2 constants exactly
 (def crypto-icons
